9af58547cc375639ddd04fc06de998f4a72a2455
fabricol <fabricioctnmiranda@gmail.com>
2024-05-03 09:46:43 -0300

    aulas 2&3, correções de erros

--- README.md
+++ README.md
@@ -4,3 +4,4 @@ Primeira aula do Módulo 23</br>
 *1.2* Essa ferramenta foi realizada utilizando JS ou Bootstrap, o arquivo main.js possue os dois passos</br>
 *1.2.1* Para realização dessa ferramenta foi necessário o entendimento de AJAX
 *1.3* Por fim foi utilizada uma aplicação de JQueryPlugin para mascarar a forma que o CEP será inserido no *input*</br>
+*2/3* Essas aulas tiveram a intenção de ensinar os erros que podem aparecer quando um pedido de front é mal requisitado, ocorrendo o erro 400.
--- main.js
+++ main.js
@@ -13,6 +13,31 @@
 // })
 
 
+// $(document).ready(function() {
+//     $('#cep').mask('00000-000')
+//     $('#btn-buscar-cep').click(function() {
+//         const cep = $('#cep').val();
+//         const endpoint = `https://viacep.com.br/ws/${cep}/json`;
+//         const botao = $(this);
+//         // this = '#btn-buscar-cep'
+//         $(botao).find('i').addClass('d-none');
+//         $(botao).find('span').removeClass('d-none');
+
+//         $.ajax(endpoint).done(function(resposta) {
+//             const logradouro = resposta.logradouro;
+//             const bairro = resposta.bairro;
+//             const cidade = resposta.localidade;
+//             const estado = resposta.uf;
+//             const endereco = `${logradouro}, ${bairro} - ${cidade} - ${estado}`;
+//             $('#endereco').val(endereco)
+            
+
+//             $(botao).find('i').removeClass('d-none');
+//             $(botao).find('span').addClass('d-none');
+//         })
+//     }) 
+// })
+
 $(document).ready(function() {
     $('#cep').mask('00000-000')
     $('#btn-buscar-cep').click(function() {
@@ -23,17 +48,26 @@ $(document).ready(function() {
         $(botao).find('i').addClass('d-none');
         $(botao).find('span').removeClass('d-none');
 
-        $.ajax(endpoint).done(function(resposta) {
-            const logradouro = resposta.logradouro;
-            const bairro = resposta.bairro;
-            const cidade = resposta.localidade;
-            const estado = resposta.uf;
-            const endereco = `${logradouro}, ${bairro} - ${cidade} - ${estado}`;
-            $('#endereco').val(endereco)
-            
-
-            $(botao).find('i').removeClass('d-none');
-            $(botao).find('span').addClass('d-none');
-        })
-    }) 
+            fetch(endpoint).then(function (resposta) {
+            return resposta.json()
+            })
+            .then(function(json){
+                const logradouro = json.logradouro;
+                const bairro = json.bairro;
+                const cidade = json.localidade;
+                const estado = json.uf;
+                const endereco = `${logradouro}, ${bairro} - ${cidade} - ${estado}`;
+                $('#endereco').val(endereco)
+            })
+            .catch(function(erro) {
+                alert("Ocorreu um erro")
+            })
+            .finally(function() {
+                setTimeout(function() {
+                    $(botao).find('i').removeClass('d-none');
+                    $(botao).find('span').addClass('d-none');
+    
+                },1000)
+            })
+    })
 })
